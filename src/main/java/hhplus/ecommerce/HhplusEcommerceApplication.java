4032f657fc3a58d39444ed542ff2e33c5f37d58c
kjh <devkjh418@gmail.com>
2024-10-08 01:10:52 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/hhplus/ecommerce/HhplusEcommerceApplication.java
@@ -0,0 +1,13 @@
+package hhplus.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusEcommerceApplication.class, args);
+    }
+
+}
