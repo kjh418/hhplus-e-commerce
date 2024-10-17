00e2c518237317ee71fc29ddf7878707219cef10
kjh <devkjh418@gmail.com>
2024-10-18 03:52:34 +0900

    feat/test : [#13] 장바구니 API 추가 - 기존에 있었던 Option은 구현 이슈로 제외처리 - 같은 상품을 장바구니에 담을 때 수량 증가

--- src/main/java/hhplus/ecommerce/domain/cart/Cart.java
+++ src/main/java/hhplus/ecommerce/domain/cart/Cart.java
@@ -1,20 +1,27 @@
 package hhplus.ecommerce.domain.cart;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Entity
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long userId;
-
     private LocalDateTime createdAt;
+
+    public Cart(Long userId) {
+        this.userId = userId;
+        this.createdAt = LocalDateTime.now();
+    }
 }
--- src/main/java/hhplus/ecommerce/infrastructure/repository/CartRepository.java
+++ src/main/java/hhplus/ecommerce/infrastructure/repository/CartRepository.java
@@ -3,5 +3,8 @@ package hhplus.ecommerce.infrastructure.repository;
 import hhplus.ecommerce.domain.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserId(Long userId);
 }
