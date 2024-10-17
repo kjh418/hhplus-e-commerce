10a1e7515cb921aea4ec147f3b0f8d288a34cd05
kjh <devkjh418@gmail.com>
2024-10-18 00:23:32 +0900

    fix : [#11] 결제 내역도 history에서 확인할 수 있도록 추가

--- src/main/java/hhplus/ecommerce/application/payment/PaymentService.java
+++ src/main/java/hhplus/ecommerce/application/payment/PaymentService.java
@@ -3,12 +3,11 @@ package hhplus.ecommerce.application.payment;
 import hhplus.ecommerce.domain.order.OrderStatus;
 import hhplus.ecommerce.domain.order.Orders;
 import hhplus.ecommerce.domain.payment.Payment;
+import hhplus.ecommerce.domain.payment.PaymentHistory;
 import hhplus.ecommerce.domain.payment.PaymentStatus;
+import hhplus.ecommerce.domain.payment.PointType;
 import hhplus.ecommerce.domain.user.Users;
-import hhplus.ecommerce.infrastructure.repository.OrdersRepository;
-import hhplus.ecommerce.infrastructure.repository.PaymentRepository;
-import hhplus.ecommerce.infrastructure.repository.UserPointRepository;
-import hhplus.ecommerce.infrastructure.repository.UsersRepository;
+import hhplus.ecommerce.infrastructure.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +24,7 @@ public class PaymentService {
     private final UsersRepository usersRepository;
     private final UserPointRepository userPointRepository;
     private final OrdersRepository orderRepository;
+    private final PaymentHistoryRepository paymentHistoryRepository;
 
     @Transactional
     public PaymentResponse processPayment(Long orderId, Long userId, BigDecimal paymentAmount) {
@@ -57,8 +57,14 @@ public class PaymentService {
             return new PaymentResponse("포인트가 부족합니다.", PaymentStatus.FAILED);
         }
 
+        saveSuccessfulPaymentHistory(userId, paymentAmount);
         processSuccessfulPayment(userId, order, paymentAmount, currentPoints);
         return new PaymentResponse("결제가 완료되었습니다.", PaymentStatus.SUCCESS);
+    }
+
+    private void saveSuccessfulPaymentHistory(Long userId, BigDecimal paymentAmount) {
+        PaymentHistory history = new PaymentHistory(userId, paymentAmount, PointType.USE, LocalDateTime.now());
+        paymentHistoryRepository.save(history);
     }
 
     private Users getUserOrThrow(Long userId) {
