58d972bef6d247760e3f026536b5dbf0dfb45d89
kjh <devkjh418@gmail.com>
2024-10-22 01:41:52 +0900

    fix : [#18] 장바구니 동시성 제어 추가 - 비관적 락 추가(많은 사용자가 접근하고 수정하는 경우, 데이터 충돌이 자주 발생할 것으로 예상되는 경우)

--- src/main/java/hhplus/ecommerce/infrastructure/repository/CartItemRepository.java
+++ src/main/java/hhplus/ecommerce/infrastructure/repository/CartItemRepository.java
@@ -1,7 +1,10 @@
 package hhplus.ecommerce.infrastructure.repository;
 
 import hhplus.ecommerce.domain.cart.CartItem;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,5 +12,8 @@ import java.util.Optional;
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     List<CartItem> findByCartId(Long id);
 
+    // 비관적락
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM CartItem c WHERE c.cartId = :id AND c.productId = :productId")
     Optional<CartItem> findByCartIdAndProductId(Long id, Long productId);
 }
