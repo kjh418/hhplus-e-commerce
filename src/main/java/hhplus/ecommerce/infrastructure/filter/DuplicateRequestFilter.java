30f26412cff8ccb2206d5b4a4e62da556748c064
kjh <devkjh418@gmail.com>
2024-10-25 02:58:29 +0900

    fix : [#21] 빈 중복 등록으로 인한 오류 수정 - DuplicateRequestFilter의 ``@Component` 어노테이션 제거 - FilterConfig에서 FilterRegistrationBean을 사용해 빈 수동 등록

--- src/main/java/hhplus/ecommerce/infrastructure/filter/DuplicateRequestFilter.java
+++ src/main/java/hhplus/ecommerce/infrastructure/filter/DuplicateRequestFilter.java
@@ -3,13 +3,11 @@ package hhplus.ecommerce.infrastructure.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-@Component
 public class DuplicateRequestFilter implements Filter {
 
     private final Map<String, Long> requestCache = new ConcurrentHashMap<>(); // ConcurrentHashMap으로 변경
