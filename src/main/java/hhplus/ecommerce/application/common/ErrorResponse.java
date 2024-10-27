20fd8ebc0f3392ae1c6829b8e267072c4bd402fc
kjh <devkjh418@gmail.com>
2024-10-27 16:42:25 +0900

    feat : [#26] ErrorResponse 개선, JSON 정렬화 추가 - `@Getter`를 추가하여 불변 객체로 설정하여 객체 접근성 향상 - `@JsonInclude(JsonInclude.Include.NON_NULL)`를 추가하여 null 값 필드 직렬화 방지

--- src/main/java/hhplus/ecommerce/application/common/ErrorResponse.java
+++ src/main/java/hhplus/ecommerce/application/common/ErrorResponse.java
@@ -1,12 +1,14 @@
 package hhplus.ecommerce.application.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
     private String errorCode;
     private String message;
