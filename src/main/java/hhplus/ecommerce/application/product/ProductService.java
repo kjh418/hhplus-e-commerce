52ef823a6d9de84c5abfdbb010ef2b77dc87a648
kjh <devkjh418@gmail.com>
2024-11-07 21:04:11 +0900

    feat : [#31] 상품 서비스에 캐싱 추가 - Redis 기반의 캐시를 사용하여 빈번한 DB 조회를 줄이는 형식으로 성능 최적화

--- src/main/java/hhplus/ecommerce/application/product/ProductService.java
+++ src/main/java/hhplus/ecommerce/application/product/ProductService.java
@@ -8,6 +8,7 @@ import hhplus.ecommerce.domain.product.Product;
 import hhplus.ecommerce.infrastructure.repository.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -20,18 +21,21 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Cacheable(value = "productListCache", key = "'allProducts'", unless = "#result.isEmpty()")
     public List<ProductListDto> getProductList() {
         List<Product> products = productRepository.findAll();
         return products.stream().map(product -> new ProductListDto(product.getId(), product.getName(), product.getPrice()))
                 .collect(Collectors.toList());
     }
 
+    @Cacheable(value = "productDetailsCache", key = "#productId")
     public ProductDetailDto getProductDetails(Long productId) {
         Product product = productRepository.findById(productId).orElseThrow(() -> new EntityNotFoundException(ErrorCode.PRODUCT_NOT_FOUND.getMessage()));
 
         return new ProductDetailDto(product.getId(), product.getName(), product.getPrice(), product.getStockQuantity(), product.getDescription());
     }
 
+    @Cacheable(value = "topProductsCache", key = "#days + '-' + #limit", unless = "#result.isEmpty()")
     public List<TopProductResponse> getTopProducts(int days, int limit) {
         LocalDateTime endDate = LocalDateTime.now(); // 현재 날짜
         LocalDateTime startDate = endDate.minusDays(days);
